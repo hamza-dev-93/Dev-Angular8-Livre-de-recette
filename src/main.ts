bede0ab4fba4f5e80e4d6ca3eac9c2a5d4ab0cc3
hamza-dev-93 <hamza@bedwihamza.com>
2019-11-29 21:47:37 +0100

    2 commit add workts

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,15 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker('./.worker', { type: 'module' });
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
